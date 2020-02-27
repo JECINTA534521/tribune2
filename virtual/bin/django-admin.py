85c7594b7a6afd81d81b414d7be2cfc3b2561dab
jecinta <jecintawanjirug@gmail.com>
2020-02-27 10:18:50 +0300

    creating the project and app

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/jecinta/Documents/mc24 projects/The-Moringa-Tribune/virtual/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
